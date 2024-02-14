cd9c073d3d9f949fdd959b5ce69076afce99f9d1
Volodymyr Donets <volodons@gmail.com>
2024-02-14 16:21:00 +0200

    Add Input property in Housing Location component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrl: './housing-location.component.css'
+  imports: [CommonModule],
+  template: ` <p>housing-location works!</p> `,
+  styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
