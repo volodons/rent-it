b4a1f2e177ca52fe3367638e3624556a077ca058
Volodymyr Donets <volodons@gmail.com>
2024-02-14 16:05:14 +0200

    Create and implement interface for Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -19,4 +22,17 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   `,
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
