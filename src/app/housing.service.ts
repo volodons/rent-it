7670cadfb5e5350696f32bb914ca8bdd9f380bb2
Volodymyr Donets <volodons@gmail.com>
2024-02-15 14:20:29 +0200

    Add Angular form for booking of housing

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -121,5 +121,9 @@ export class HousingService {
     );
   }
 
-  constructor() {}
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`
+    );
+  }
 }
